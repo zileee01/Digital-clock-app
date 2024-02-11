34ca3c6bddf196fa6d57710b0dd86eb3de6d147d
Zineb <mz_atir@esi.dz>
2024-02-11 22:05:13 +0100

    first commit

---
+++ src/DigitalClock.jsx
@@ -0,0 +1,44 @@
+import React , {useState , useEffect} from "react";
+
+function DigitalClock (){
+
+    const [time , setTime] = useState(new Date());
+    
+    useEffect(() => {
+        const intervalleId = setInterval(() => {
+          setTime(new Date());   
+        } ,1000); 
+
+        return () => {
+            clearInterval(intervalleId); 
+        }
+    }, []); 
+
+    function formatTime () {
+        let hours = time.getHours(); 
+        const minutes = time.getMinutes(); 
+        const seconds = time.getSeconds(); 
+        const meridiem = hours >= 12 ? "PM" : "Am"; 
+
+        hours = hours % 12 || 12 ; 
+
+        return `${padZero(hours)}:${padZero(minutes)}:${padZero(seconds)} ${meridiem}`; 
+
+    }
+
+    function padZero(number){
+        return (number < 10 ? "0" : "") + number;  
+    }
+
+return (
+    <div className="clock-container">
+        <div className="clock">
+
+            <span>{formatTime()}</span>
+
+        </div>
+    </div>
+)
+}
+
+export default DigitalClock
